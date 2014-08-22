06d5976975228b35493d0be5bd55914164985eec
maayalee <maayalee@gmail.com>
2014-08-22 20:43:21 +0900

    Test

--- src/trunk/android/aadt/src/com/github/aadt/plugins/keyboard/Keyboard.java
+++ src/trunk/android/aadt/src/com/github/aadt/plugins/keyboard/Keyboard.java
@@ -2,11 +2,13 @@ package com.github.aadt.plugins.keyboard;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View.*;
 import android.view.inputmethod.*;
 
 import com.github.aadt.kernel.actor.Actor;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView.*;
 
 import com.github.aadt.kernel.util.*;
@@ -20,9 +22,11 @@ import android.text.*;
 public class Keyboard extends Actor {
   private Activity activity;
   private EditText text;
+  private UnityPlayer unity_player;
 
-  public Keyboard(Activity arg_activity) {
+  public Keyboard(Activity arg_activity, UnityPlayer unit_player) {
     this.activity = arg_activity;
+    this.unity_player = unit_player;
   }
 
   public void start() {
@@ -31,25 +35,19 @@ public class Keyboard extends Actor {
         RelativeLayout.LayoutParams.WRAP_CONTENT);
     localLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
     text = new EditText(activity);
-    text.setLayoutParams(localLayoutParams);
-    // text.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+    // text.setLayoutParams(localLayoutParams);
+    text.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
     // text.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-    text.setInputType(InputType.TYPE_NULL);
-    text.setOnFocusChangeListener(new OnFocusChangeListener() {
-      @Override
-      public void onFocusChange(View arg0, boolean arg1) {
-        Logger.debug("FOCUS CHANGE");
-        InputMethodManager inputMgr = (InputMethodManager) activity
-            .getSystemService(Context.INPUT_METHOD_SERVICE);
-        /*
-         * inputMgr.toggleSoftInput(InputMethodManager.SHOW_FORCED,
-         * InputMethodManager.HIDE_IMPLICIT_ONLY);
-         */
-        text.setInputType(InputType.TYPE_CLASS_TEXT);
-        text.requestFocus();
-        inputMgr.showSoftInput(text, InputMethodManager.SHOW_IMPLICIT);
-      }
-    });
+    /*
+     * text.setInputType(InputType.TYPE_NULL); text.setOnFocusChangeListener(new
+     * OnFocusChangeListener() {
+     * 
+     * @Override public void onFocusChange(View arg0, boolean arg1) { Logger.debug("FOCUS CHANGE");
+     * InputMethodManager inputMgr = (InputMethodManager) activity
+     * .getSystemService(Context.INPUT_METHOD_SERVICE);
+     * text.setInputType(InputType.TYPE_CLASS_TEXT); text.requestFocus();
+     * inputMgr.showSoftInput(text, InputMethodManager.SHOW_IMPLICIT); } });
+     */
     /*
      * text.setOnEditorActionListener(new OnEditorActionListener() {
      * 
@@ -75,28 +73,60 @@ public class Keyboard extends Actor {
      * view.getRootView().getHeight(); int h2 = view.getHeight(); Logger.debug("height1 is " + h1);
      * Logger.debug("height2 is " + h2); } });
      */
-    register_text_view();
+    // add_linear_layout();
   }
 
   public void show() {
     Logger.debug("Keyboard::show");
+    add_relative_layout();
   }
 
-  private void register_text_view() {
+  private void add_relative_layout() {
     activity.runOnUiThread(new Runnable() {
       @Override
       public void run() {
-        RelativeLayout localRelativeLayout = new RelativeLayout(activity);
+        RelativeLayout layout = new RelativeLayout(UnityPlayer.currentActivity
+            .getApplicationContext());
+        RelativeLayout.LayoutParams params;
+        params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+            RelativeLayout.LayoutParams.MATCH_PARENT);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+        //layout.setBackgroundColor(Color.BLUE);
+
+        RelativeLayout localRelativeLayout = new RelativeLayout(UnityPlayer.currentActivity
+            .getApplicationContext());
+        // localRelativeLayout.setGravity(Gravity.BOTTOM);
         RelativeLayout.LayoutParams localLayoutParams;
         localLayoutParams = new RelativeLayout.LayoutParams(
             RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        localLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        // localLayoutParams.height = 1000;
+        //localLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        localLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         localRelativeLayout.setLayoutParams(localLayoutParams);
+        localRelativeLayout.setBackgroundColor(Color.RED);
         localRelativeLayout.addView(text);
+        
+        layout.addView(localRelativeLayout);
 
-        // activity.getWindow().addContentView(localRelativeLayout, localLayoutParams);
-        activity.addContentView(localRelativeLayout, localLayoutParams);
-        // activity.addContentView(text, localLayoutParams);
+        UnityPlayer.currentActivity.addContentView(layout, params);
+      }
+    });
+  }
+
+  private void add_linear_layout() {
+    UnityPlayer.currentActivity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        LinearLayout localLinearLayout = new LinearLayout(UnityPlayer.currentActivity
+            .getApplicationContext());
+        localLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        localLinearLayout.setGravity(Gravity.BOTTOM);
+        LinearLayout.LayoutParams localLayoutParams;
+        localLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT);
+        // localLinearLayout.setBackgroundColor(Color.RED);
+        localLinearLayout.addView(text);
+        UnityPlayer.currentActivity.addContentView(localLinearLayout, localLayoutParams);
       }
     });
   }
